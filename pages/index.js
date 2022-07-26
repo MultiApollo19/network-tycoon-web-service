4b63bf68e67084f6a47f554f145ec001627913f7
MultiApollo19 <mariusz.belwon@gmail.com>
2022-07-27 00:02:57 +0200

    Suprabase

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
+import { supabase } from '../suprabase/clientApp'
+import Auth from '../components/suprabase/auth'
+import Account from '../components/suprabase/account'
+
+export default function Home() {
+
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tornado | Home</title>
+        <meta name="description" content="Managment app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+    </div>
+    </div>
+  )
+}
