e0281d91034f2218535ecc4ca08e2ff4c625cdbc
MultiApollo19 <mariusz.belwon@gmail.com>
2022-07-27 23:48:31 +0200

    fixes

--- next.config.js
+++ next.config.js
@@ -2,14 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
   env:{
     GITHUB_SECRET: process.env.GITHUB_SECRET,
     GITHUB_ID: process.env.GITHUB_ID,
     DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+  
 }
 
 module.exports = nextConfig
