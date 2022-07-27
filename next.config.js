bdc659a91bab5c1a17f92d93d8af817d1666c710
MultiApollo19 <mariusz.belwon@gmail.com>
2022-07-27 23:32:22 +0200

    Working profile

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+  env:{
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    GITHUB_ID: process.env.GITHUB_ID,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import Layout from "../components/layout";
 import "../styles/globals.css"
+import { SessionProvider } from "next-auth/react";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps:{session, ...pageProps} }) {
   return (
+    <SessionProvider session={session}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </SessionProvider>
+    
   );
 }
