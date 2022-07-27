ea317f0e903a8f7e355f1fc0051c43b290d95f38
MultiApollo19 <mariusz.belwon@gmail.com>
2022-07-27 22:03:05 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env:{
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    GITHUB_ID: process.env.GITHUB_ID,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 }
 
 module.exports = nextConfig
