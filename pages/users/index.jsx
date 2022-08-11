70a996047794dd8185a6895cf4b01f125058237f
MultiApollo19 <mariusz.belwon@gmail.com>
2022-08-11 16:19:02 +0200

    Api stage 1
    Users list and user details

---
+++ pages/users/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Users() {
+
+  return (
+    <div className="users">
+      <Head>
+        <title>Tornado | Users</title>
+        <meta name="description" content="Managment app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
--- prisma/migrations/20220729154545_role/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('ADMIN', 'DEMO');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'DEMO';
