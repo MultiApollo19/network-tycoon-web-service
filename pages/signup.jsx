a21cd9920b856f8716786d1be64a1f141a5bfb86
MultiApollo19 <mariusz.belwon@gmail.com>
2022-08-26 17:08:17 +0200

    Update signup.jsx

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -5,9 +5,6 @@ import { useRouter } from 'next/router';
 import { useState } from "react";
 
 import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-
-import 'react-toastify/dist/ReactToastify.css';
 
 export default function SignUpPage(){
     const router = useRouter();
